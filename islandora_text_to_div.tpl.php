ea4f7477b6acd9050b3c3db07616612e824d707f
William Panting <willtp87@hotmail.com>
2012-05-17 13:36:07 -0300

    first

---
+++ islandora_text_to_div.tpl.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file
+ *   This file is the template to show list containing text data.
+ *
+ * @param array text_data
+ */
+
+?>
+<div class="islandora_text_to_div_viewer">
+<?php
+  print $text_data;
+?>
+</div>
